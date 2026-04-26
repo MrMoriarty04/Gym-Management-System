a14b74605e19187d67100d0b424a1ec480e5959d
mousaG <mousa@gmail>
2026-04-26 23:19:53 +0300

    Initial commit: Setup MERN stack for Gym Management System and complate schema

---
+++ backend/models/Diet.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const dietSchema = new mongoose.Schema({
+ traineeId:{type:mongoose.Schema.Types.ObjectId, ref:'User'},
+  date:{type:Date ,required:true},
+targetedDiet:{
+    calories: { type: Number ,required:true},
+    protein: { type: Number ,required:true},
+    carbs: { type: Number ,required:true},
+    fat: { type: Number ,required:true},
+},
+meals:[{
+mealName:{ type: String ,required:true},
+ingredients:{ type: String ,required:true},
+//!from AI
+aiCalculatedCalories:{ type: Number,default :0},
+aiCalculatedProtein:{ type: Number,default :0},
+aiCalculatedCarbs:{ type: Number,default :0},
+aiCalculatedFat:{ type: Number,default :0},
+}],
+
+totalConsumed:{
+ calories:{ type: Number,default :0},
+protein:{ type: Number,default :0},
+carbs:{ type: Number,default :0},
+fat:{ type: Number,default :0},
+
+}
+
+},
+{ timestamps: true });
+
+module.exports = mongoose.model('Diet', dietSchema);
---
+++ backend/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+ name:{type:String ,required:true},
+ email:{type:String ,required:true},
+ password:{type:String ,required:true},
+ role:{type:String,enum: ['Admin', 'Coach', 'Trainee'], default: 'Trainee'},
+ otp  :{type:String},
+ otpExpires:{type:Date},
+ coachId: { 
+  type: mongoose.Schema.Types.ObjectId, 
+  ref: 'User' 
+},
+subscription: {
+    plan: { type: Number, enum: [1, 3, 6, 12] },
+    startDate: { type: Date },
+    endDate: { type: Date },
+    isActive: { type: Boolean, default: false }
+  }
+}, 
+{ timestamps: true }); 
+
+module.exports = mongoose.model('User', userSchema);
